53b762650bbc65397e9397a25866e107042ea6c8
Aim4 <aimforking@gmail.com>
2020-08-19 18:47:51 -0400

    Add skeleton for TrackControls

---
+++ src/components/TrackControls.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TrackControls extends Component {
+    render() {
+        return (
+            <div className="track-controls">
+                I am track controls
+            </div>
+        );
+    }
+}
+
+export default TrackControls;
