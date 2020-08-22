eaf5115dfacb3fed03aa22f262ef089beeed282f
Aim4 <aimforking@gmail.com>
2020-08-22 17:45:52 -0400

    Add output for volume and bpm

--- src/components/MainControls.js
+++ src/components/MainControls.js
@@ -22,12 +22,14 @@ class MainControls extends Component {
             <div className="MainControls">
                 Controls here
                 <div className="slide-container">
-                    <input type="range" min="0" max="100" value={this.state.volume} step="1" className="slider" name="volume" onChange={this.onVolumeChange}></input>
                     <label htmlFor="volume">Volume</label>
+                    <input type="range" min="0" max="100" value={this.state.volume} step="1" className="slider" name="volume" onChange={this.onVolumeChange}></input>
+                    <output name="volume-output" htmlFor="volume">{this.state.volume}</output>
                 </div>
                 <div className="slide-container">
-                    <input type="range" min="0" max="160" value={this.state.bpm} step="10" className="slider" name="bpm" onChange={this.onBPMChange}></input>
                     <label htmlFor="bpm">Beats Per Minute</label>
+                    <input type="range" min="0" max="160" value={this.state.bpm} step="10" className="slider" name="bpm" onChange={this.onBPMChange}></input>
+                    <output name="bpm-output" htmlFor="bpm">{this.state.bpm}</output>
                 </div>
             </div>
         );
